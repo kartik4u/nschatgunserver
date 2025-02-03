aaf7047a64c318847ba9a5023b007b93430b8b4f
narinder <9999enjoy@gmail.com>
2025-02-03 19:20:10 +0530

    changes

--- server.js
+++ server.js
@@ -11,14 +11,27 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(cors());
 
 let users = {};
+let usernames = [];
+let boxes = {};
 
 io.on("connection", (socket) => {
   console.log("User Connected:", socket.id);
 
-  socket.on("join-room", (room, username) => {
+  socket.on("join-room", (room, username,initialData) => {
     socket.join(room);
     users[socket.id] = { username, room };
-    io.to(room).emit("user-joined", `${username} joined the chat`);
+
+    usernames.push(username);
+    console.log("room",room);
+    boxes[room] = boxes[room]?boxes[room]:[];
+    boxes[room].push(initialData);
+
+    //boxes[room][username]=initialData;
+    //.push(initialData);
+    // let enData = {allData:boxes[room]}
+    //boxes.push(initialData);
+    console.log("boxes",boxes);
+    io.to(room).emit("user-joined", `${username} joined the chat`,usernames,boxes[room]);
   });
 
   socket.on("send-message", (messageData) => {
@@ -29,8 +42,21 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     const user = users[socket.id];
     if (user) {
-      io.to(user.room).emit("user-left", `${user.username} left the chat`);
+
+
+
+      let id = usernames.indexOf(user.username);
+      usernames.splice(id,1);
+
+      let idBox = boxes[user.room].findIndex((v,i)=>v.username==user.username);
+      boxes[user.room].splice(idBox,1);
+
+      io.to(user.room).emit("user-left", `${user.username} left the chat`,user,usernames,boxes[user.room]);
       delete users[socket.id];
+
+
+
+      //delete users[socket.id];
     }
     console.log("User Disconnected:", socket.id);
   });
